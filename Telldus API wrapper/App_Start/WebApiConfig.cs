b8f5812db9804f985895c9c81acf788692119674
mariussm <marius.msm@gmail.com>
2016-12-04 13:45:53 +0100

    Add project files.

---
+++ Telldus API wrapper/App_Start/WebApiConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace Telldus_API_wrapper.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
